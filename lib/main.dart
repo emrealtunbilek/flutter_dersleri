85061288db71ba7ab9648bc6139d3803e148fa2f
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-02 02:11:21 +0300

    row column odev ve çözümü

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'row_column_odev.dart';
 
 void main() {
   runApp(
@@ -6,80 +8,23 @@ void main() {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.cyan, accentColor: Colors.orange),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Flutter Dersleri",
-            style: TextStyle(fontSize: 24.0, color: Colors.white),
+          appBar: AppBar(
+            title: Text(
+              "Flutter Dersleri",
+              style: TextStyle(fontSize: 24.0, color: Colors.white),
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint("FAB tıklandı");
-          },
-          //backgroundColor: Colors.green,
-          child: Icon(
-            Icons.access_alarms,
-            color: Colors.white,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              debugPrint("FAB tıklandı");
+            },
+            //backgroundColor: Colors.green,
+            child: Icon(
+              Icons.access_alarms,
+              color: Colors.white,
+            ),
           ),
-        ),
-        body: Container(
-          color: Colors.red,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                    child: Icon(
-                      Icons.add_circle,
-                      size: 64,
-                      color: Colors.yellow,
-                    ),
-                  color: Colors.blue.shade200,
-                ),
-                flex: 2,
-              ),
-
-              Expanded(
-                child: Container(
-                  child: Icon(
-                    Icons.add_circle,
-                    size: 64,
-                    color: Colors.yellow,
-                  ),
-                  color: Colors.green.shade200,
-                ),
-                flex: 2,
-              ),
-
-              Expanded(
-                child: Container(
-                  child: Icon(
-                    Icons.add_circle,
-                    size: 64,
-                    color: Colors.yellow,
-                  ),
-                  color: Colors.purple.shade200,
-                ),
-                flex: 4,
-              ),
-
-              Expanded(
-                child: Container(
-                  child: Icon(
-                    Icons.add_circle,
-                    size: 64,
-                    color: Colors.yellow,
-                  ),
-                  color: Colors.orange.shade200,
-                ),
-                flex: 6,
-              ),
-
-            ],
-          ),
-        ),
-      ),
+          body: RowColumnOdevCozumu()),
     ),
   );
 }
