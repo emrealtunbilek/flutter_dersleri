b48701de2ad6d78adf329fb61ef3ab8a817466fe
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-02 20:52:37 +0300

    fadeinimage ce instrictheight anlatıldı

--- lib/main.dart
+++ lib/main.dart
@@ -8,78 +8,105 @@ void main() {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.cyan, accentColor: Colors.orange),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Flutter Dersleri",
-              style: TextStyle(fontSize: 24.0, color: Colors.white),
-            ),
+        appBar: AppBar(
+          title: Text(
+            "Flutter Dersleri",
+            style: TextStyle(fontSize: 24.0, color: Colors.white),
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              debugPrint("FAB tıklandı");
-            },
-            //backgroundColor: Colors.green,
-            child: Icon(
-              Icons.access_alarms,
-              color: Colors.white,
-            ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB tıklandı");
+          },
+          //backgroundColor: Colors.green,
+          child: Icon(
+            Icons.access_alarms,
+            color: Colors.white,
           ),
-          body: Column(
-            children: <Widget>[
-              Text(
-                "Resim ve Buton Türleri",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Row(
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              "Resim ve Buton Türleri",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            IntrinsicHeight(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red.shade200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Image.asset("assets/images/emre.jpg"),
-                        Text("Asset Image"),
-                      ],
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset("assets/images/emre.jpg"),
+                          Text("Asset Image"),
+                        ],
+                      ),
                     ),
                   ),
-
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red.shade200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
-                        Text("Network Image"),
-                      ],
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.network(
+                              "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                          Text("Network Image"),
+                        ],
+                      ),
                     ),
                   ),
-
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red.shade200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: NetworkImage("https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
-                          backgroundColor: Colors.purple,
-                          radius: 30,
-                          foregroundColor: Colors.orange,
-                        ),
-                        Text("Circle Avatar"),
-                      ],
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
+                            backgroundColor: Colors.purple,
+                            radius: 30,
+                            foregroundColor: Colors.orange,
+                          ),
+                          Text("Circle Avatar"),
+                        ],
+                      ),
                     ),
                   ),
                 ],
-              )
-            ],
-          )),
+              ),
+            ),
+
+            IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          FadeInImage.assetNetwork(placeholder: "assets/images/loading.gif", image: "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                          Text("FadeinImage"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
   );
 }
