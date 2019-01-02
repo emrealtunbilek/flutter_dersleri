ef2015d905928803cca48f8f839b4d89872fce31
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-02 03:51:32 +0300

    asset network ve circle avatar resim türleri anlatıldı

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,62 @@ void main() {
               color: Colors.white,
             ),
           ),
-          body: RowColumnOdevCozumu()),
+          body: Column(
+            children: <Widget>[
+              Text(
+                "Resim ve Buton Türleri",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.asset("assets/images/emre.jpg"),
+                        Text("Asset Image"),
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                        Text("Network Image"),
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: NetworkImage("https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
+                          backgroundColor: Colors.purple,
+                          radius: 30,
+                          foregroundColor: Colors.orange,
+                        ),
+                        Text("Circle Avatar"),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     ),
   );
 }
