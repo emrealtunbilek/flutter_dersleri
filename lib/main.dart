2551bac2dc79d31e0ec86f99a8b06a39fbe868af
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-05 13:09:52 +0300

    stateful widgeti anlayalım bölüm 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,45 @@
-import 'ui/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main()  => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      title: "MaterialApp Title",
+      home: MyHomePage(title: "My Home Page2",),
+    );
+  }
+}
 
+class MyHomePage extends StatelessWidget {
 
+  final String title;
 
+  MyHomePage({this.title});
 
-
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Butona Basılma Sayısı :"),
+            Text("5",style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),)
+          ],
+        ),
+      ),
+    );
+  }
+}
