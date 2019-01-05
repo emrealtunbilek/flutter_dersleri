d2e4c7a233f37c92afbc2b3d0cb51e17db43042d
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-05 13:46:21 +0300

    stateful widgeti anlayalım bölüm 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,34 +12,68 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       title: "MaterialApp Title",
-      home: MyHomePage(title: "My Home Page2",),
+      home: MyHomePage(
+        title: "My Home Page2",
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-
+class MyHomePage extends StatefulWidget {
   final String title;
+  int sayac = 0;
 
-  MyHomePage({this.title});
+  MyHomePage({this.title}){
+    debugPrint("MyHomePage Statefull Widger constructor");
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    debugPrint("MyHomePage create state tetiklendi");
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+
+  MyHomePageState(){
+    debugPrint("MyHomePageState constructor tetiklendi");
+  }
 
   @override
   Widget build(BuildContext context) {
+    debugPrint("MyHomePageState  build methodu tetiklendi");
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            sayacDegeriniArtir();
+            debugPrint("sayac degeri arttırıldı set state cagrıldı");
+          });
+        },
+        child: Icon(Icons.add),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text("Butona Basılma Sayısı :"),
-            Text("5",style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),)
+            Text(
+              "${widget.sayac}",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )
           ],
         ),
       ),
     );
+  }
+
+  void sayacDegeriniArtir() {
+    widget.sayac++;
+    debugPrint("Sayac degeri : ${widget.sayac}");
   }
 }
