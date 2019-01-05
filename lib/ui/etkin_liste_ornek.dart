06d2ec1373feb6ca4ae12d895320d385232249b2
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-06 02:55:25 +0300

    alert dialog ve SingleChildScrollView kullanımı

--- lib/ui/etkin_liste_ornek.dart
+++ lib/ui/etkin_liste_ornek.dart
@@ -2,78 +2,118 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class EtkinListeOrnek extends StatelessWidget {
-
-  List<Ogrenci> tumOgrenciler=[];
+  List<Ogrenci> tumOgrenciler = [];
 
   @override
   Widget build(BuildContext context) {
-
     ogrenciVerileriniGetir();
 
     return ListView.separated(
-      separatorBuilder: (context, index) {
-
-        if(index % 4 == 0 && index != 0){
-          return Container(
-            height: 2, color: Colors.blue, margin: EdgeInsets.all(40),
+        separatorBuilder: (context, index) {
+          if (index % 4 == 0 && index != 0) {
+            return Container(
+              height: 2,
+              color: Colors.blue,
+              margin: EdgeInsets.all(40),
+            );
+          } else {
+            return Container();
+          }
+        },
+        itemCount: 20,
+        //itemBuilder: (context, index) => Card(),
+        itemBuilder: (context, index) {
+          return Card(
+            color:
+                index % 2 == 0 ? Colors.red.shade200 : Colors.orange.shade200,
+            elevation: 4,
+            child: ListTile(
+              leading: Icon(Icons.perm_contact_calendar),
+              title: Text(tumOgrenciler[index]._isim),
+              subtitle: Text(tumOgrenciler[index]._aciklama),
+              trailing: Icon(Icons.add),
+              onTap: () {
+                debugPrint(tumOgrenciler[index].toString());
+                toastMesajGoster(index, false);
+                alertDialogGoster(context, index);
+              },
+              onLongPress: () {
+                debugPrint("Uzun basılan eleman $index");
+                toastMesajGoster(index, true);
+              },
+            ),
           );
-        }else{
-          return Container(
-
-          );
-        }
-
-      } ,
-      itemCount: 20,
-      //itemBuilder: (context, index) => Card(),
-      itemBuilder: (context, index){
-        return Card(
-          color: index%2 == 0 ? Colors.red.shade200 : Colors.orange.shade200,
-          elevation: 4,
-          child: ListTile(
-            leading: Icon(Icons.perm_contact_calendar),
-            title: Text(tumOgrenciler[index]._isim),
-            subtitle: Text(tumOgrenciler[index]._aciklama),
-            trailing: Icon(Icons.add),
-            onTap: (){
-              debugPrint("Secilen eleman $index");
-              Fluttertoast.showToast(
-
-                  msg: "Secilen eleman $index",
-                  toastLength: Toast.LENGTH_LONG,
-                  gravity: ToastGravity.BOTTOM,
-                  timeInSecForIos: 1,
-                  textColor: Colors.black
-              );
-            },
-            onLongPress: (){
-              debugPrint("Uzun basılan eleman $index");
-              Fluttertoast.showToast(
-                  msg: "Uzun basılan eleman $index",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.CENTER,
-                  timeInSecForIos: 1,
-                  backgroundColor: Colors.blue.shade400,
-                  textColor: Colors.black
-              );
-            },
-          ),
-        );
-      }
-    );
+        });
   }
 
-  void ogrenciVerileriniGetir(){
-    tumOgrenciler=List.generate(300, (index)=>Ogrenci("Ogrenci $index Adı", "Ogrenci $index açıklaması", index%2 ==0 ? true : false));
+  void ogrenciVerileriniGetir() {
+    tumOgrenciler = List.generate(
+        300,
+        (index) => Ogrenci("Ogrenci $index Adı", "Ogrenci $index açıklaması",
+            index % 2 == 0 ? true : false));
   }
 
+  void alertDialogGoster(BuildContext ctx, int index) {
+    showDialog(
+        context: ctx,
+        barrierDismissible: false,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text("Alert Dialog Başlığı"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text("Öğrenci adı : ${tumOgrenciler[index]._isim}"),
+                  Text("Öğrenci adı : ${tumOgrenciler[index]._aciklama}"),
+                ],
+              )
+            ),
+            actions: <Widget>[
+              ButtonTheme.bar(
+                child: ButtonBar(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text("Tamam"),
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("Kapat"),
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  void toastMesajGoster(int index, bool uzunBasilma) {
+    Fluttertoast.showToast(
+        msg: uzunBasilma
+            ? "uzun basildi :" + tumOgrenciler[index].toString()
+            : "Tek tıklama " + tumOgrenciler[index].toString(),
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1,
+        textColor: Colors.black);
+  }
 }
 
-class Ogrenci{
+class Ogrenci {
   String _isim;
   String _aciklama;
   bool _cinsiyet;
 
   Ogrenci(this._isim, this._aciklama, this._cinsiyet);
 
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "Secilen öğrenci adı:$_isim açiklaması:$_aciklama";
+  }
 }
