f656a9cda0953f416997cb5f388bc3beed769643
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-08 02:06:52 +0300

    slivergrid ve çeşitleri anlatıldı

--- lib/ui/custom_scroll_ve_slivers.dart
+++ lib/ui/custom_scroll_ve_slivers.dart
@@ -9,12 +9,12 @@ class CollapsableToolbarOrnek extends StatelessWidget {
       slivers: <Widget>[
         SliverAppBar(
           backgroundColor: Colors.red,
-          expandedHeight: 100,
+          expandedHeight: 200,
           floating: false,
           pinned: true,
           snap: false,
           flexibleSpace: FlexibleSpaceBar(
-            title: Text("Sliver App Bar"),
+            title: Text("Custom Scrollview App"),
             centerTitle: true,
             background: Image.asset(
               "assets/images/emre.jpg",
@@ -22,6 +22,40 @@ class CollapsableToolbarOrnek extends StatelessWidget {
             ),
           ),
         ),
+
+
+
+        //sabit elemanlarla bir satırda kaç eleman olacağını söylediğimiz grid türü
+        SliverGrid(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          delegate: SliverChildListDelegate(sabitListeElemanlari()),
+        ),
+
+        //dinamik (builder ile üretilen) elemanlarla bir satırda kaç eleman olacağını söylediğimiz grid türü
+        SliverGrid(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          delegate: SliverChildBuilderDelegate(_dinamikElemanUretenFonksiyon,
+              childCount: 6),
+        ),
+
+        //dinamik (builder ile üretilen) elemanlarla bir satırdaki bir elemanın max genişliğini söylediğimiz grid türü
+        SliverGrid(
+          gridDelegate:
+              SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200),
+          delegate: SliverChildBuilderDelegate(_dinamikElemanUretenFonksiyon,
+              childCount: 6),
+        ),
+
+
+        SliverGrid.count(crossAxisCount: 6, children:
+          sabitListeElemanlari()
+        ,),
+
+        SliverGrid.extent(maxCrossAxisExtent: 300, children: sabitListeElemanlari(),),
+
+        //statik elemanlı bir sliver list view
         SliverPadding(
           padding: EdgeInsets.all(4),
           sliver: SliverList(
@@ -31,6 +65,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
           ),
         ),
 
+        //elemanları builder ile olusturulan list view
         SliverPadding(
           padding: EdgeInsets.all(10),
           sliver: SliverList(
@@ -39,13 +74,22 @@ class CollapsableToolbarOrnek extends StatelessWidget {
           ),
         ),
 
-        SliverFixedExtentList(delegate: SliverChildListDelegate(sabitListeElemanlari()), itemExtent: 100),
+        //elemanları statik olusturulan ama extent mantıgıyla calısan listview
+        SliverFixedExtentList(
+            delegate: SliverChildListDelegate(sabitListeElemanlari()),
+            itemExtent: 100),
 
-        SliverFixedExtentList(delegate: SliverChildBuilderDelegate(_dinamikElemanUretenFonksiyon,childCount: 6), itemExtent: 50,),
+        //elemanları dinamik olusturulan ama extent mantıgıyla calısan listview
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(_dinamikElemanUretenFonksiyon,
+              childCount: 6),
+          itemExtent: 50,
+        ),
       ],
     );
   }
 
+  //listeler için tanımlanmış elemanları döndüren fonksiyon
   List<Widget> sabitListeElemanlari() {
     return [
       Container(
@@ -111,6 +155,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
     ];
   }
 
+  //listeler için dinamik olarak eleman döndüren fonksiyon
   Widget _dinamikElemanUretenFonksiyon(BuildContext context, int index) {
     return Container(
       height: 100,
@@ -124,6 +169,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
     );
   }
 
+  //liste elemanları için rastgele renk üretme fonksiyonu
   Color rastgeleRenkUret() {
     return Color.fromARGB(
         matematik.Random().nextInt(255),
