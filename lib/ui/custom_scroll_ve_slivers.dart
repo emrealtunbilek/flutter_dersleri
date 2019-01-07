2e2f81810a7e3fde5e9386d12bca12fffe4a0086
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-08 00:38:10 +0300

    sliverlist ve sliverlisteye statik olarak eleman ekleme anlatıldı

--- lib/ui/custom_scroll_ve_slivers.dart
+++ lib/ui/custom_scroll_ve_slivers.dart
@@ -5,24 +5,67 @@ import 'package:flutter/material.dart';
 class CollapsableToolbarOrnek extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return CustomScrollView(
-
-    slivers: <Widget>[
-      SliverAppBar(
-        title: Text("Sliver App Bar"),
-        backgroundColor: Colors.red,
-        expandedHeight: 200,
-        floating: true,
-      ),
-     /* SliverList(),
-      SliverFixedExtentList(),
-      SliverGrid(),
-      SliverGrid.count(crossAxisCount: 2),
-      SliverGrid.extent(maxCrossAxisExtent: 200),*/
-    ],
-
+      slivers: <Widget>[
+        SliverAppBar(
+          backgroundColor: Colors.red,
+          expandedHeight: 300,
+          floating: true,
+          pinned: true,
+          snap: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text("Sliver App Bar"),
+            centerTitle: true,
+            background: Image.asset("assets/images/emre.jpg", fit: BoxFit.cover,),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            sabitListeElemanlari(),
+          ),
+        ),
+      ],
     );
   }
 
+  List<Widget> sabitListeElemanlari() {
+    return [
+      Container(
+        height: 100,
+        color: Colors.amber,
+        alignment: Alignment.center,
+        child: Text("Sabit Liste Elemanı 1",style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+      ),
+      Container(
+        height: 100,
+        color: Colors.teal,
+        alignment: Alignment.center,
+        child: Text("Sabit Liste Elemanı 2",style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+      ),
+      Container(
+        height: 100,
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: Text("Sabit Liste Elemanı 3",style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+      ),
+      Container(
+        height: 100,
+        color: Colors.orange,
+        alignment: Alignment.center,
+        child: Text("Sabit Liste Elemanı 4",style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+      ),
+      Container(
+        height: 100,
+        color: Colors.purple,
+        alignment: Alignment.center,
+        child: Text("Sabit Liste Elemanı 5",style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+      ),
+      Container(
+        height: 100,
+        color: Colors.cyan,
+        alignment: Alignment.center,
+        child: Text("Sabit Liste Elemanı 6",style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
+      ),
+    ];
+  }
 }
