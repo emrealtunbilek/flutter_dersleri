f6415351919965b9cc7bbd6b9a503ba560eee631
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-07 03:20:48 +0300

    boxdecoration ve decorationimage anlatıldı

--- lib/ui/grid_view_kullanimi.dart
+++ lib/ui/grid_view_kullanimi.dart
@@ -6,14 +6,38 @@ class GridViewOrnek extends StatelessWidget {
     return GridView.builder(
       itemCount: 100,
       gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       itemBuilder: (BuildContext context, int index) {
         return Container(
-          alignment: Alignment.center,
-          color: Colors.teal[100 * ((index+1) % 8)],
-          child: Text(
-            "Merhaba Flutter $index",
-            textAlign: TextAlign.center,
+          alignment: Alignment.bottomCenter,
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.orange,width: 10),
+              //borderRadius: new BorderRadius.all(new Radius.circular(5.0)),
+              boxShadow: [
+                new BoxShadow(
+                  color: Colors.red,
+                  offset: new Offset(10.0, 5.0),
+                  blurRadius: 20.0,
+                )
+              ],
+              shape: BoxShape.circle,
+              color: Colors.blue[100 * ((index + 1) % 8)],
+              gradient: LinearGradient(
+                  colors: [Colors.yellow, Colors.red],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter),
+              image: DecorationImage(
+                  image: AssetImage("assets/images/emre.jpg"),
+                  fit: BoxFit.contain,
+                  alignment: Alignment.topCenter)),
+          margin: EdgeInsets.all(20),
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Merhaba Flutter $index",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
           ),
         );
       },
